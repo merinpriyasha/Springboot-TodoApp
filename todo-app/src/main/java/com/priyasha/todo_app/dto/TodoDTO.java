d8a2418e28dd6504fbb7bb70a2fe6e2e48b52206
merinpriyasha <merinpriyasha@gmail.com>
2024-11-01 19:36:52 +0530

    database configuration

--- todo-app/src/main/java/com/priyasha/todo_app/dto/TodoDTO.java
+++ todo-app/src/main/java/com/priyasha/todo_app/dto/TodoDTO.java
@@ -1,4 +1,34 @@
 package com.priyasha.todo_app.dto;
 
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoDTO {
+
+    @Id
+    private Long id;
+
+    @NotBlank(message = "Task name cannot be empty")
+    private String task;
+
+    @NotBlank(message = "Status cannot be empty")
+    @Pattern(regexp = "^(pending|ongoing|completed)$", message = "Status must be either 'pending', 'ongoing' or 'completed'")
+    //the status field can only have one of the values: "pending", "completed", or "ongoing"
+    private String status;
+
+    @NotNull(message = "Deadline date cannot be null")
+    @Future(message = "Deadline date must be in the future")
+    private LocalDate deadlineDate = LocalDate.now().plusDays(1); // sets the default value of deadlineDate to the next day from the current date
+
 }
--- todo-app/src/main/java/com/priyasha/todo_app/model/Todo.java
+++ todo-app/src/main/java/com/priyasha/todo_app/model/Todo.java
@@ -1,4 +1,39 @@
 package com.priyasha.todo_app.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "todos") //specifies the table name in db
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto increment id
+    private Long id;
+
+    @NotBlank(message = "Task name cannot be empty")
+    @Column(name = "task", nullable = false) //Map the field to the db column
+    private String task;
+
+    @NotBlank(message = "Status cannot be empty")
+    @Pattern(regexp = "^(pending|ongoing|completed)$", message = "Status must be either 'pending', 'ongoing' or 'completed'")
+    @Column(name = "status", nullable = false) //Map the field to the db column
+    private String status;
+
+    @NotNull(message = "Deadline date cannot be null")
+    @Future(message = "Deadline date must be in the future")
+    @Column(name = "deadline_date", nullable = false) //Map the field to the db column
+    private LocalDate deadlineDate;
+
 }
